9c15e4794c8e891ff562601fddc98f74093f4fe1
Muhsin310302 <Muhsin310302@hotmail.com>
2023-02-14 15:55:00 +0100

    version 2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 import ContentPage from './components/ContentPage';
 import Header from './components/Header';
@@ -5,12 +6,23 @@ import Nav from './components/Nav';
 import './css/main.css'
 
 
+const cartStart =[
+  {
+    id: 0,
+    title: "",
+    price: 0,
+
+  }
+]
+
 function App() {
+  const [cartItem, setCartItems] = useState([...cartStart]) 
+
   return (
     <div className="container">
         <Header/>
         <Nav />
-        <ContentPage/>
+        <ContentPage cartItem={cartItem} setCartItems={setCartItems}/>
     </div>
   );
 }
--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,14 +1,21 @@
 import products from '../products.json'
 import Product from './Product'
 import Title from './Title'
-export default function ContentPage({name}){
+export default function ContentPage({name, cartItem, setCartItems}){
     return (
         <main>
             <Title/>
             <div id="product-list">
             {products.map((product) =>{
              return(
-                <Product key={product.prodid} name={product.title} price={product.price} cat={product.category} img={product.imagefile} />
+                <Product key={product.prodid}
+                 name={product.title} 
+                 price={product.price} 
+                 cat={product.category}
+                 img={product.imagefile}
+                 cartItems={cartItems} 
+                 setCartItems={setCartItems}
+                 proid={product.proid}/>
                 )
                 } 
             )}
